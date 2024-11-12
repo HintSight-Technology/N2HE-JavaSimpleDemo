a17b3d56739d7b751966bad104e46c8cc64ea7b9
Kaiwen <70592332+kai981@users.noreply.github.com>
2024-11-12 16:55:57 +0800

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -10,28 +10,5 @@ public class Main {
         int id = 14;
         String filepath = "src/X_test.csv";
         LogisticRegression.predict(filepath, numberOfUsers, numberOfRows, id);
-
-//        List<String[]> list = new ArrayList<>();
-//        Path path = null;
-//        try {
-//            path = Paths.get(ClassLoader.getSystemResource("UCI_Credit_Card.csv").toURI());
-//        } catch (URISyntaxException e) {
-//            throw new RuntimeException(e);
-//        }
-//        try (Reader reader = Files.newBufferedReader(path)) {
-//            try (CSVReader csvReader = new CSVReader(reader)) {
-//                String[] line;
-//                while ((line = csvReader.readNext()) != null) {
-//                    list.add(line);
-//                }
-//            } catch (CsvValidationException e) {
-//                throw new RuntimeException(e);
-//            }
-//        } catch (IOException e) {
-//            throw new RuntimeException(e);
-//        }
-//
-//        System.out.println(list.toString());
-
     }
 }
